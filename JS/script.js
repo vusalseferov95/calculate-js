4becbbc4d5034590df3fed5920ff5bf1d6e8b61e
vusalseferov95 <vusal.seferov8595@gmail.com>
2024-03-25 16:58:24 +0400

    second-commit

--- JS/script.js
+++ JS/script.js
@@ -1,37 +1,75 @@
 const display = document.querySelector('#display');
-const calculate = document.querySelector('#calculate');
+const calculateElement = document.querySelector('#calculate');
 
 let displayNumber = 0;
+let operator = '';
+let firstValue = '';
+let selectOperator = false;
 
 const displayView = () => {
     display.value = displayNumber;
 }
 
 const updateDisplay = (value) => {
-    if (displayNumber === 0) displayNumber = value;
-    else displayNumber += value;
+    if (displayNumber === 0 || selectOperator) displayNumber = value.toString();
+    else displayNumber += value.toString();
 }
 
 const clearDisplay = () => {
     displayNumber = 0;
+    operator = '';
+    firstValue = '';
+    selectOperator = false;
 }
 
-const dotDisplay = (value) => {
-    if (!displayNumber.includes(value)) {
+const dotDisplay = (value) => {  
+    if (!displayNumber.toString().includes(value)) {
         displayNumber += value;
     }
 }
 
+const operatorView = (value, oldValue) => {
+    selectOperator = true;
+    operator = value;
+    firstValue = displayNumber;
+}
+
+const calculate = () => {
+
+     if operator === '+' {
+        displayNumber = parseFloat(displayNumber) + parseFloat(firstValue);
+     }
+     else if (operator === '-') {
+        displayNumber = parseFloat(displayNumber) - parseFloat(firstValue);
+     }
+     else if (operator === '/') {
+        displayNumber = parseFloat(displayNumber) / parseFloat(firstValue);
+     }
+     else if (operator === '*') {
+        displayNumber = parseFloat(displayNumber) * parseFloat(firstValue);
+     }
+
+     displayView();
+}
+
 displayView(); 
 
-calculate.addEventListener('click', e => {
+calculateElement.addEventListener('click', e => {
     if (e.target.matches ('button')) {
         const value = e.target.value;
         if (e.target.classList.contains ('operator')) {
-            console.log('operator')
+            operatorView(value)
+
+            if (value === '=') {
+                calculate();
+            }
+            else {
+                operatorView(value);
+                displayView();
+            }
         }
         else if (e.target.classList.contains ('dot')) {
-            dotDisplay(value);
+            dotDisplay(value, displayNumber);
             displayView();
         }
         else if (e.target.classList.contains ('clear')) {
